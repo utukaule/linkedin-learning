7899144c90cead2317ac764f6b25c27f3ab0964f
Somashekar9999 <97974767+Somashekar9999@users.noreply.github.com>
2022-02-22 18:04:33 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,135 @@
+let arr = [
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4D0DAQEpEdHoiYkBiQ/learning-public-crop_144_256/0/1592866184809?e=1645617600&v=beta&t=72f9cmkyF2JgXVsFRIp06EPJOd-AXVapQvglew_jTHc", //img path
+    title: "Learning Python (2020)", // title
+    level: "Advanced",
+    creater: "Joe Marini", //owner
+    date: "Sep 2020", // data
+    decription:
+      "Python—the popular and highly readable object-oriented language—is both powerful and relatively easy to learn. Whether you're new to programming or an experienced developer, this course can help you get started with Python. Joe Marini provides an overview of the installation process, basic Python syntax, and an example of how to construct and run a simple Python program. Learn to work with dates and times, read and write files, and retrieve and parse HTML, JSON, and XML data from the web", // description
+    no_learner: "108,952", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQHwIrkpQ442GQ/learning-original-video-vbr-540/0/1607643942134?e=1645617600&v=beta&t=Txq6MUY20EHkQ9zBucjgGkGnfWMVrqcnJ9cFmc05ilY#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4D0DAQFU4LTXLPJaIA/learning-public-crop_675_1200/0/1592866069779?e=1645617600&v=beta&t=QUEJjsN9tOWr_LswKlr2r-rHD4UpRhNpvV4axJVOZbE", //img path
+    title: "Python Essential Training", // title
+    level: "Beginner",
+    creater: "Bill Weinman", //owner
+    date: "Oct 2020", // data
+    decription:
+      "Due to its power and simplicity, Python has become the scripting language of choice for many large organizations, including Google, Yahoo, and IBM. A thorough understanding of Python 3, the latest version, will help you write more efficient and effective scripts. In this course, Bill Weinman demonstrates how to use Python 3 to create well-designed scripts and maintain existing projects. This course covers the basics of the language syntax and usage, as well as advanced features such as objects, generators, and exceptions. Learn how types and values are related to objects; how to use control statements, loops, and functions; and how to work with generators and decorators. Bill also introduces the Python module system and shows examples of Python scripting at work in a real-world application.", // description
+    no_learner: "527,410", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQHX0ogkiIe9Ew/learning-original-video-vbr-540/0/1598819420719?e=1645617600&v=beta&t=nJaL5E__QtuTnMUb_q3PRnIFCUsvuEHvvViKBkvR9-8#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4D0DAQFfhs2EuPDcuA/learning-public-crop_675_1200/0/1592866667823?e=1645617600&v=beta&t=Pr4Ht0JgENqgie-MHT0Aw7JVy3rjKag2jPObPfm9vH8", //img path
+    title: "SQL Essential Training", // title
+    level: "Beginner",
+    creater: "Bill Weinman", //owner
+    date: "Oct 2020", // data
+    decription:
+      "Knowing how to code and manage relational databases and database-driven applications is a valuable skill for any career in tech. Completely updated for 2019, SQL Essential Training is designed to help users understand the most common language for database wrangling, SQL. All the lessons have been recorded in SQLiteStudio, an intuitive and easy-to-install database manager. Instructor Bill Weinman teaches all the major features of SQL: creating tables; defining relationships; manipulating strings, numbers, and dates; using triggers to automate actions; and using subselects and views. He offers a solid working knowledge of the language and shows how to retrieve and manage data efficiently. The final chapter includes a real-world example of building a simple application using SQL", // description
+    no_learner: "511,315", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C560DAQEpis__jso9LA/learning-original-video-vbr-540/0/1599168749861?e=1645617600&v=beta&t=D596hs-79NA3MsUdA0MxZJEpLvCVkpkOCOggcb3RIAA#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQE36TRRFU6eEw/learning-public-crop_675_1200/0/1568665816099?e=1645617600&v=beta&t=GeWRJXN4EUuwOEGY6ddGhpWY_fMNG_0xdf-kRt68Jtw", //img path
+    title: "Programming Foundations: Fundamentals", // title
+    level: "Beginner",
+    creater: "Annyce Davis", //owner
+    date: "oct 2020", // data
+    decription:
+      "Gain the core knowledge to begin programming in any language. In this course, instructor Annyce Davis uses Python to explore the core concepts and structure of programming languages and helps you grasp what’s going on under the hood. After going over basic statements and expressions in Python, Annyce explores strings, variables, and conditional code—which are common topics in all programming languages. She also shows how to work with different kinds of data and troubleshoot a variety of errors. Along the way, she compares how code is written in several different languages and provides guidance on the criteria to use when choosing a programming language. Upon wrapping up this course, you’ll have the knowledge you need to continue your coding journey in whichever language piques your interest", // description
+    no_learner: "366,433", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQHDUpArl1xv9Q/learning-original-video-vbr-540/0/1598695175646?e=1645617600&v=beta&t=EAOirsUmsAno1PUArrcQQGHTVEO3Ar75jHK4fnih3Q0#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQGZq3yHFuR9mg/learning-public-crop_675_1200/0/1568060053948?e=1645617600&v=beta&t=rgKiTcbevr5oLpjwXWuQcBxAmxVCjD1PfDpjs8jmJLM", //img path
+    title: "Learning MATLAB", // title
+    level: "Beginner + Intermediate",
+    creater: "Steven Moser", //owner
+    date: "Dec 2020", // data
+    decription:
+      "MATLAB is one of the most popular programming languages today for engineers and scientists, and with good reason-it allows analysts to work efficiently with large amounts of data. Instructor Steven Moser shows you how to harness the MATLAB tools and create programs to model your own data and hypotheses. First, learn how to create basic variables and cell arrays, and work with the core MATLAB syntax, which is significantly different than other programming languages. Then discover how to create scripts and functions, work with matrices, debug your program, and import data. To illustrate your findings, Steven shows how to create 2D and 3D plots, add annotations, and incorporate images. The final chapter covers Simulink, the MATLAB block diagram tool", // description
+    no_learner: "296,466", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQE2AgT1opUoWg/learning-original-video-vbr-540/0/1598521021422?e=1645617600&v=beta&t=vJ65Dce6tloI8p5cdNOOuaLCG4dMvu9T6rqZDS3MTvA#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQHqNko91KVUGQ/learning-public-crop_675_1200/0/1575308053139?e=1645617600&v=beta&t=daT7UmpFZdxMquTW3xXcGiSzfrb-1Iy1rBgyvcy_47A", //img path
+    title: "SQL: Data Reporting and Analysis", // title
+    level: "Intermediate",
+    creater: "Emma Saunders", //owner
+    date: "Nov 2020", // data
+    decription:
+      "Do you rely on IT to get the data you need? Are you often stuck waiting in line for data, and wish you could just retrieve it yourself? In this course, learn how to get the data you want by writing a bit of SQL code. You won't just be able to pull data out of the database; you'll be able to manipulate it: merging it, grouping it, and relabeling it to get just the report you want. Join Emma Saunders as she shows how to write simple SQL queries for data reporting and analysis using a publicly accessible online database. Learn how to filter, group, and sort data, using built-in SQL functions to format or calculate results. Discover how to perform more complex queries, such as joining data together from different database tables. Last but not least, she introduces views, procedures, functions, and variables", // description
+    no_learner: "250,218", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C560DAQHYAZEimBmkGg/learning-original-video-vbr-540/0/1599110497603?e=1645617600&v=beta&t=a-GkCN9Vw9iTbySViVRRFdiMMkM2bpGHzQQHL6z7hDk#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQF_kbCzFxeYsw/learning-public-crop_675_1200/0/1572372542713?e=1645617600&v=beta&t=GA2ZTBElPsH7PgkYsCrNj6Pdm6J4jtsZqMK7h3HZzao", //img path
+    title: "Using Python with Excel", // title
+    level: " Intermediate",
+    creater: "Anthony Sagely", //owner
+    date: "Aug 2020", // data
+    decription:
+      "Python is the world's fastest growing programming language. It's a powerful tool that is helping to streamline development and data science. In this course from Madecraft, you can learn how to combine the power of the Python language with the data analysis and reporting tools in Microsoft Excel. Discover how to leverage the pandas and openpyxl libraries to inspect, filter, clean, and convert data and how to build solid reports that help businesses take action. Discover how to work with large amounts of data that would be unmanageable in Excel alone. By the end of this course, viewers will learn how this combination of tools can lead businesses to better insights than ever before", // description
+    no_learner: "250,218", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQE90EPDLNNvsQ/learning-original-video-vbr-540/0/1598959175090?e=1645617600&v=beta&t=gEZ2okQFOHFMYHvcZzPOQzn12mxq9u_lWzHHy5P6ILM#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQG3bGexV7b4AA/learning-public-crop_675_1200/0/1619134533264?e=1645621200&v=beta&t=Dq7bdxJODNu-rl90y8bO57xg7SXmKEg6aKI2_wCAqIs", //img path
+    title: "Python: Working with Predictive Analytics", // title
+    level: " Advanced",
+    creater: "Isil Berkun", //owner
+    date: "Aug 2020", // data
+    decription:
+      "Data can tell many stories: where it came from and where it’s going. Predictive analytics gives programmers a tool to tell stories about the future: to extract usable information and make accurate predictions. These predictions, in turn, allow business to make more informed, impactful decisions. Join Isil Berkun, data scientist, to explore predictive analytics with Python. Discover how to prepare data—fill in missing values, perform feature scaling, and more—and use prebuilt Python libraries to make and evaluate prediction models. She describes what models to use when, and explains the concepts in such a way that you can immediately apply them to your own work. By the end of the course, you’ll be able to leverage Python libraries like pandas and NumPy and choose the right prediction models for your projects", // description
+    no_learner: "248,216", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C560DAQFmzZKSrgC3CQ/learning-original-video-vbr-540/0/1599168432337?e=1645621200&v=beta&t=EwcnKigK23qrRfxfsV_DLqGZQ8vBv-xOsjuVBKBUTnw#.mp4", // video link here,
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQHgyyvv2PhNpw/learning-public-crop_675_1200/0/1571768552078?e=1645621200&v=beta&t=NhW13KRs0LwBJCbeYkqSkQD-zbT8jNPmgUt5Hd9z3-g", //img path
+    title: "Python for Data Science Essential Training Part 1", // title
+    level: "Intermediate",
+    creater: "Lillian Pierson, P.E.", //owner
+    date: "Aug 2020", // data
+    decription:
+      "Python for Data Science Essential Training is one of the most popular data science courses at LinkedIn Learning. It has now been updated and expanded to two parts—for even more hands-on experience with Python. In this course, instructor Lillian Pierson takes you step by step through a practical data science project: a web scraper that downloads and analyzes data from the web. Along the way, she introduces techniques to clean, reformat, transform, and describe raw data; generate visualizations; remove outliers; perform simple data analysis; and generate interactive graphs using the Plotly library. You should walk away from this training with basic coding experience that you can take to your organization and quickly apply to your own custom data science projects", // description
+    no_learner: "201,686", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4E0DAQGO9lSdIzf92w/learning-original-video-vbr-540/0/1598836649697?e=1645621200&v=beta&t=cBbySnj5MvmeeNAXqnmBOFL9qJXgE3bIsBQUUmG-0i8#.mp4", // video link here,
+  },
+
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQE9UaR5VrPBng/learning-public-crop_675_1200/0/1568669504962?e=1645621200&v=beta&t=5mTPajCNylElSqK-fhg7dlwyI4evDgdZSk4Yc-Uf4wc", //img path
+    title: "Scripting for Testers", // title
+    level: "Intermediate",
+    creater: "Dave Westerveld", //owner
+    date: "Aug 2020", // data
+    decription:
+      "Scripting can expand your abilities as a tester and help you keep up with the pace of agile. It allows you to more quickly and efficiently find software issues and automate manual tasks such as reporting and test site preparation. This course examines a few ways to use scripts to solve common challenges faced in the fast-paced world of software testing. Follow along and learn how to navigate and test APIs, generate reports, explore products with web automation tools, generate dummy data for testing, and more. The lessons rely on simple Python-based scripts as well as popular testing tools such as Google Sheets and Selenium WebDriver. Instructor Dave Westerveld provides concrete examples and ideas to increase your productivity, eliminate boring tasks, build new insights into products, and standardize and simplify your team's work ",
+    no_learner: "201,686",
+    video_link:
+      " https://dms.licdn.com/playlist/C560DAQHEB9FhdQzhYQ/learning-original-video-vbr-540/0/1598869113012?e=1645621200&v=beta&t=ppnrLoR6IEeZkzp9BfRtYUrlEO6A-VGVb33166qV8us#.mp4",
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C4E0DAQHHioAJ_NOcbA/learning-public-crop_288_512/0/1617301744692?e=1645617600&v=beta&t=ND_bbB6nFwqToRPFgbaDRDeZMp7Jw_6XY6NZvgBTK3w", //img path
+    title: "Learning Anime", // title
+    level: "Intermediate",
+    creater: "Maryam Hanafiah", //owner
+    date: "April 12, 2021", // data
+    decription:
+      "For JavaScript developers, creating animation through CSS properties can seem alien, while specifying every detail in JavaScript can be verbose.  Anime.js provides JavaScript developers with an easy set of tools for creating animations by scripting CSS and other changes. In this course, instructor Maryam Hanafiah explains how to animate HTML and SVG through JavaScript, including techniques to polish your animations. Maryam shows you how to get started animating simple objects, manipulating animation values, and defining animation parameters. She covers how to use easing and staggering for more natural movement, then concludes with a discussion on adding basic animation controls and scrolling controls.", // description
+    no_learner: "215,216", //random number
+    video_link:
+      "https://dms.licdn.com/playlist/C4D0DAQGmO6N27ZY37A/learning-original-video-vbr-720/0/1616096982151?e=1645617600&v=beta&t=tvZ-V2VQUaWpcKQQFYFtrd8oG-U5FvPJ6lutJZHY8p4", // video link here,
+  },
+];
