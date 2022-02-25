7e040a96e64f02ad6d88c51aa4ab3b31df11a6e4
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-02-25 12:16:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,114 @@
+let arr = [
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4D0DAQEVToFoHO0heg/learning-public-crop_144_256/0/1592866798214?e=1645610400&v=beta&t=DnS6d_Kb-7H3yhXPoOGZx5JyuA5HBXB3vH0hYI3pSbg", //img path
+        title:"CSS Essential Training", // title
+        level:"Easy",
+        creater: "Christina Truong", //owner
+        date:"Oct 2019", // data
+        decription:"Cascading Style Sheets (CSS) is a stylesheet language that allows you to control the appearance of your webpages. In this hands-on course, Christina Truong demonstrates the concepts that form the foundation of CSS, explaining what you need to know to tweak existing CSS and write your own. Christina explains how to add colors and other design elements to take your webpages beyond just black text on a white background. She shows how to use selectors, how the box model defines the spacing and sizing of page elements, and how to style text and manage basic layouts with Flexbox and Grid. She also covers working with advanced selectors, creating fluid layouts, and determining when to use the float and position properties. Plus, at the end of the course, you'll walk away with an actual project—an online résumé page.", // description
+        no_learner: "30,465 learners", //random number
+        video_link: "https://dms.licdn.com/playlist/C560DAQEzNpz5W9ZMcQ/learning-original-video-vbr-720/0/1614297068284?e=1645614000&v=beta&t=1VJbq_yuGsSEMHNhAJ5sujCV6C4Xu0xATorAe46hzYE" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C560DAQEiVxZXG6qWcQ/learning-public-crop_144_256/0/1627923772402?e=1645614000&v=beta&t=SRc8rh8LTYIB_fQQfOB4VvmjbsuRZiLf9KJmUJ32MgY", //img path
+        title:"CSS: Combining Grid and Flexbox", // title
+        level:"Advanced",
+        creater: "Emily Kay", //owner
+        date:"August 4, 2021", // data
+        decription:"With Grid and Flexbox, CSS is changing dramatically. Whereas in the past, creating complex layouts required all sorts of CSS hacks, JavaScript, or both, now you can use Flexbox and Grid—tools designed specifically for layouts—to create responsive web pages with precise, custom layouts. In this course, Emily Kay covers the basics of Flexbox and Grid separately, from syntaxes to properties to how they work in general. After that, she details instances where you may need to use both to accomplish your layout goals. As Emily shows, when you use Flexbox and Grid together to combine their separate strengths, you can create and display complex web content in a beautiful, user-friendly way.", // description
+        no_learner: "2,813 learners", //random number
+        video_link: "https://dms.licdn.com/playlist/C4E0DAQFY9cFFccTVSw/learning-original-video-vbr-720/0/1626807337798?e=1645614000&v=beta&t=8UyzA1FY29nQlgrQHRil7OcfU2s75Ww2Ypg3WkDHzNs" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQEp_MXOLC0Sfw/learning-public-crop_144_256/0/1568666828740?e=1645614000&v=beta&t=MSHo13Z14ZVXbhPUbbVhmAEznO2oKQWDe1FJHq-xsBk", //img path
+        title:"CSS Shorts", // title
+        level:"Intermediate",
+        creater: "Chris Converse", //owner
+        date:"Dec 2018", // data
+        decription:"CSS Shorts is a series covering all aspects of CSS, including properties, preprocessors, positioning, floats, and the most up-to-date styling techniques. Instructor Chris Converse returns every Tuesday, bringing you short and timely tips that will help new and experienced web developers alike create more beautiful websites and more efficient code. Tune in and learn how to take more control over the style, layout, and even the user experience of your web designs.", // description
+        no_learner: "3,489", //random number
+        video_link: "https://dms.licdn.com/playlist/C4D0DAQEY-IDuthhLGg/learning-original-video-vbr-720/0/1598845052043?e=1645614000&v=beta&t=qQSeK_vNYmDEgiTcxc29eFtisJOyxFQoNl2AfL-UvHQ" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQFYWtT8RGNRSA/learning-public-crop_144_256/0/1639417840343?e=1645614000&v=beta&t=BVhVMm3NeKjb7t9Ne2Np0abP6BwapuTBC0yR9ph-uvI", //img path
+        title:"Cutting-Edge CSS", // title
+        level:"Intermediate",
+        creater: "Morten Rand-Hendriksen", //owner
+        date:"Oct 2018", // data
+        decription:"CSS evolves rapidly, and it can be difficult to stay on top of the most recent modules, features, and best practices. In this course, Morten Rand-Hendriksen aims to fill this gap by riding the bleeding edge of CSS to showcase what’s new, what’s possible, and how to use cutting edge CSS in a safe way in production. To do this, Morten uses one of the least-cutting edge methods possible, paper. But by cutting away as much distraction as possible, Morten helps you visualize how CSS code relates to the actual browser output, which is key to mastering CSS.", // description
+        no_learner: "2,460", //random number
+        video_link: "https://dms.licdn.com/playlist/C560DAQE1_SsHkGm3bQ/learning-original-video-vbr-720/0/1638293171806?e=1645614000&v=beta&t=lPbX6ZXIrftzxUOsI6ouWVHSht4niVSFFAEtdXtckcU" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C560DAQGDQVR5XWNRTA/learning-public-crop_144_256/0/1613059570026?e=1645614000&v=beta&t=llbZkquRjwtDc-GW3lV4ZwjIXIZ6Y5zP6SXPH6NHoKY", //img path
+        title:"CSS:Images", // title
+        level:"Basic",
+        creater: "Morten Rand-Hendriksen", //owner
+        date:"Aug 15", // data
+        decription:"CSS is a toolkit for image manipulation. Whether you want to create distinctive visual effects or simply make the most of available bandwidth, the options for manipulating images in CSS will let you create reusable graphics solutions. In this course, instructor Morten Rand-Hendriksen shows you how much you can edit images in a browser using pure CSS. Morten explains the importance of proper HTML image markup and how to crop, fit, and position an image. He discusses how to use the clip-path property to create a variety of shapes. Morten shows how you can use and combine filters to edit and add effects to your image. He concludes by walking you through several practical examples of how to use what you learned, as well as some important best practices and caveats", // description
+        no_learner: "4,456", //random number
+        video_link: "https://dms.licdn.com/playlist/C560DAQG_1sU2yhzdGA/learning-original-video-vbr-720/0/1611784443235?e=1645614000&v=beta&t=9kNAX4aTzu6Ob-_mIO0CVw06UzyHGCK5LI7Enp5q-7c" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQHliL819SNhiA/learning-public-crop_144_256/0/1568668686910?e=1645614000&v=beta&t=jymKv4ZrhHbO_iHM2C0EtgnI_WbnRRqGUvctIWBK6lY", //img path
+        title:"CSS Layouts: From Float to Flexbox and Grid", // title
+        level:"Advance",
+        creater: "Christina Truong", //owner
+        date:"Sep 2019", // data
+        decription:"For decades, CSS developers fiddled with floats to create flexible layouts that worked across browsers. But their brilliant hack had a lot of downsides. The latest generation of CSS specs offers a better and vastly more intuitive set of tools, but moving from floats to Grid, Flexbox, or both means adjusting how you think. In this course, Christina Truong guides you though this process, from initial concepts to complete conversion, highlighting the different ways to create page layouts with modern CSS. Explore how to work with the display and float properties; use relative, absolute, and fixed positioning for laying out components; create basic Grid and Flexbox layouts; and more.", // description
+        no_learner: "3,545", //random number
+        video_link: "https://dms.licdn.com/playlist/C4E0DAQGdJCUBQpzSxw/learning-original-video-vbr-720/0/1599169428172?e=1645614000&v=beta&t=AnxHyBW7rZYHAKsV1BY3tFhefi0cesABN5EbKbhnFJI" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQFiMj9Bl4IeXg/learning-public-crop_144_256/0/1619129848790?e=1645614000&v=beta&t=t0q31JL6nO8YUOC3nMG-W3bwg01XZTeZEStjwkylme4", //img path
+        title:"CSS: Advanced Layouts with Grid", // title
+        level:"Advance",
+        creater: "Morten Rand-Hendriksen", //owner
+        date:"Jan 2019 ", // data
+        decription:"Excel PivotTables–which allow you to quickly calculate and analyze large amounts of data–are one of the most powerful features of Microsoft’s ubiquitous spreadsheet tool. But many users have only a basic understanding of how PivotTables work. In this course, instructor Jess Stratton offers a plethora of bite-sized tips designed to deepen your understanding of how PivotTables work and how to use them more effectively.Learn how to properly format data and create a basic PivotTable in Excel. Discover ways to adjust, rearrange, and group data to calculate and summarize values that you can put to use on the job. Get tips on data filtering and sorting, conditional formatting, and using PivotCharts and slicers. Along the way, Jess shows how to deal with common PivotTable error messages, such as invalid field names and overlapping reports. By the end of this course, you’ll be able to create more robust PivotTables in minutes—just in time for that next meeting.", // description
+        no_learner: "3,200", //random number
+        video_link: "https://dms.licdn.com/playlist/C4D0DAQF8CBKnq8hWFQ/learning-original-video-vbr-720/0/1644349222872?e=1645614000&v=beta&t=mbv-kenKLQ40BD9xh7-IZ_it4RbKg0qGqvh-GacVztU" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQEB-ZSSDj1aBw/learning-public-crop_144_256/0/1568664072843?e=1645617600&v=beta&t=QDnWaAlam0MgZtFKgN4VlEGoDPavdcp0Lg91wZ9HevE", //img path
+        title:"CSS: Selectors", // title
+        level:"Advance",
+        creater: "Jen Kramer", //owner
+        date:"Feb 2019", // data
+        decription:"Discover how to effectively leverage the power of selectors to select the elements you want to style without adding classes, changing HTML, or getting overly specific with your selectors. In this course, instructor Jen Kramer demonstrates how to pinpoint specific parts (and groups of parts) in an HTML document using the powerful declarative syntax of CSS selectors. Jen covers combinator selectors, attribute selectors, pseudo-class and pseudo-element selectors, and the universal selector. She also offers tips for determining which selectors might not be supported by your browser choices and shares best practices for mixing selectors in your document. And because this is an easily-tested syntax, Jen includes questions at the end of most videos to reinforce your understanding of each concept.", // description
+        no_learner: "11,469 learners", //random number
+        video_link: "https://dms.licdn.com/playlist/C560DAQEkhXGX_flo-g/learning-original-video-vbr-720/0/1638397515484?e=1645617600&v=beta&t=kKOpWCdOt00Xe5_u-YuHLpMk7jbnAEzMpbEaEg9FUKk",  
+      },
+
+        {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQEIk8dVpCGgqg/learning-public-crop_144_256/0/1624300231924?e=1645617600&v=beta&t=tNkswAtTw5VKaD2rVshwDi0FZM7nN4yBFdpCRWxIJgU", //img path
+        title:"Bootstrap 5 Essential Training", // title
+        level:"Beginner+Intermediate",
+        creater: "Ray Villalobos", //owner
+        date:"Sep 2019", // data
+        decription:"Bootstrap—a front-end framework using HTML, CSS, and JavaScript—is designed to help developers quickly and easily build responsive, mobile-ready websites that are cross-browser compatible. An open-source framework, Bootstrap features a 12-column grid and components that are ready to use. In this course, Ray Villalobos helps you get started with Bootstrap 5, providing an overview of all of the features in this popular framework. Ray covers installation options, and walks through the basic styles that normalize how your content is displayed on different platforms and browsers, before moving into detailed coverage of more advanced features, including typography and flexbox layouts. Learn how to use the layout components to create a full-featured website and make it interactive with components such as carousels, accordions, alerts, and tooltips. Plus, learn how to build beautifully designed and usable forms that validate user input.", // description
+        no_learner: "24,563", //random number
+        video_link: "https://dms.licdn.com/playlist/C4D0DAQFs_MD0N1wNtQ/learning-original-video-vbr-720/0/1624296322927?e=1645617600&v=beta&t=cDeuEMzJ1cdp70h5Cq6OWk_sH0yaN5wshGLznfm6niM" // video link here,
+
+    },
+    {
+        img:"https://media-exp1.licdn.com/dms/image/C4E0DAQEcG3xRHpyQ1w/learning-public-crop_144_256/0/1567117798247?e=1645617600&v=beta&t=eUszshb6AnuOhnEM55FbtCcNbN5WYuTjq0ngaqsqsv0", //img path
+        title:"Bootstrap 4 with Sass", // title
+        level:"Advance",
+        creater: "Ray Villalobos ", //owner
+        date:"Jan 2022", // data
+        decription:"Bootstrap is the most popular framework for building responsive, mobile-first websites. When you combine it with Sass, you get CSS superpowers and a level of customization that makes Bootstrap even more powerful. This course explores the differences between regular Bootstrap and the new Sass-driven version, Bootstrap 4, and shows how to use the Sass-enabled features to build more customized sites. Instructor Ray Villalobos shows how to install libraries to add custom fonts, icons, and animations; use Sass variables, maps, and mixins to create custom layouts; modify global settings; and customize existing components with CSS and Sass.", // description
+        no_learner: "12,348", //random number
+        video_link: "https://dms.licdn.com/playlist/C4E0DAQH1sw_Fc9_mLA/learning-original-video-vbr-720/0/1598813942470?e=1645617600&v=beta&t=dN96v75_NnGXT3-IMuVLM0zPdQ6f7R-2oHwHuZKB00w" // video link here,
+
+    },
+    
+   
+];
