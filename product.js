752d582768bb3c4cb7746e11add4629059072f90
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-02-26 18:34:40 +0530

    made some correction
    merging, Img correction etc

---
+++ product.js
@@ -0,0 +1,22 @@
+// document.querySelector('.3').innerHTML=`<div>img sr</IMG></div>
+// <div></div>`
+document.querySelector('.one').innerHTML=`<div>
+<h1>LinkedIn Learning Hub</h1>
+<br>
+<p>A Learning Experience Platform (LXP) with an active community that engages members through personalized content and insights across all of your content sources. LinkedIn Learning Hub is more than a traditional LXP it empowers your employees to develop the right skills.</h3>
+<br><br>
+<a href="#" class="contact">Contact</a> 
+<a href="#" class="learn" >Learn More</a>
+</div>`
+document.querySelector('.two').innerHTML=`
+<iframe src="https://www.youtube-nocookie.com/embed/jbsOZp1kxvE?rel=0&amp;autohide=1&amp;playsinline=0&amp;html5=1&amp;wmode=opaque&amp;autoplay=0" frameborder="0" allowfullscreen="" title="Video: LinkedIn Learning Hub"></iframe>
+`
+document.querySelector('.three').innerHTML=`
+<p3>How LinkedIn Learning helps your organization and its people build the skills they need</p3> 
+<p><span>Personalized content</span>  that matches learners' skill gaps and professional goals</p3>`
+// document.querySelector('.4').innerHTML=`
+// `
+// document.querySelector('.one').innerHTML=``
+// document.querySelector('.one').innerHTML=``
+// document.querySelector('.one').innerHTML=``
+// import navbar from "nav001.html";
