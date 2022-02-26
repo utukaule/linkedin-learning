4ab0314429353fa0531270c209c1401e8853fa46
Somashekar9999 <97974767+Somashekar9999@users.noreply.github.com>
2022-02-26 17:54:35 +0530

    Add files via upload

---
+++ Resources.js
@@ -0,0 +1,51 @@
+document.querySelector("#img1").addEventListener("click", function () {
+  window.location.href = "img1.html";
+});
+
+document.querySelector("#img2").addEventListener("click", function () {
+  window.location.href = "img2.html";
+});
+
+document.querySelector("#img3").addEventListener("click", function () {
+  window.location.href = "img3.html";
+});
+
+document.querySelector("#img4").addEventListener("click", function () {
+  window.location.href = "img4.html";
+});
+
+document.querySelector("#img5").addEventListener("click", function () {
+  window.location.href = "img5.html";
+});
+
+document.querySelector("#img6").addEventListener("click", function () {
+  window.location.href = "img6.html";
+});
+
+document.querySelector("#img7").addEventListener("click", function () {
+  window.location.href = "img7.html";
+});
+
+document.querySelector("#img8").addEventListener("click", function () {
+  window.location.href = "img8.html";
+});
+
+document.querySelector("#img9").addEventListener("click", function () {
+  window.location.href = "img9.html";
+});
+
+document.querySelector("#img10").addEventListener("click", function () {
+  window.location.href = "img10.html";
+});
+
+document.querySelector("#img11").addEventListener("click", function () {
+  window.location.href = "img11.html";
+});
+
+document.querySelector("#img12").addEventListener("click", function () {
+  window.location.href = "img12.html";
+});
+
+document.querySelector("#img13").addEventListener("click", function () {
+  window.location.href = "img13.html";
+});
---
+++ support.js
@@ -0,0 +1,15 @@
+document.querySelector("#ig1").addEventListener("click", function () {
+  window.location.href = "ig1.html";
+});
+
+document.querySelector("#ig2").addEventListener("click", function () {
+  window.location.href = "ig2.html";
+});
+
+document.querySelector("#ig3").addEventListener("click", function () {
+  window.location.href = "ig3.html";
+});
+
+document.querySelector("#ig4").addEventListener("click", function () {
+  window.location.href = "ig4.html";
+});
