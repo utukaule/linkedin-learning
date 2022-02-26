7b383bc3727542df21e666efc29a320b00aed24e
epj23Azfqw <89893187+epj23Azfqw@users.noreply.github.com>
2022-02-26 20:35:30 +0530

    postcheckout page

--- postcheckout.js
+++ postcheckout.js
@@ -3,13 +3,30 @@ var Fname = uN[0].firstName;
 var Lname = uN[0].lastName;
 
 document.querySelector("#FN").value = Fname;
+document.querySelector("#FN1").append(Fname);
 document.querySelector("#LN").value =  Lname;
 
 function monthly (){
-    document.querySelector("#month").style.color = "green";
-    ocument.querySelector("#annually").style.color = "black";
+    document.querySelector("#fl").textContent = "";
+    document.querySelector("#fr").textContent = ""; 
+    document.querySelector("#fr2").textContent = ""; 
+    document.querySelector("#fl").append("Monthly after free trial");
+    document.querySelector("#fr").append("₹1,610.17");
 }
 function annual(){
-    document.querySelector("#annually").style.color = "green";
-    document.querySelector("#month").style.color = "black";
+    document.querySelector("#fl").textContent = "";
+    document.querySelector("#fr").textContent = "";
+    document.querySelector("#fr2").textContent = "";
+    document.querySelector("#fl").append("Total after free trial (₹1,016.95 x 12 months)");
+    document.querySelector("#fr").append("₹12,203.39");
+    document.querySelector("#fr2").append("*₹7,118.65 saving when you buy annual");
+}
+
+
+
+function signOut (){
+    localStorage.removeItem("usrNames");
+    localStorage.removeItem("signInDataBase");
+    alert("you are logging out!!!");
+    window.location.href = "index.html";
 }
