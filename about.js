3c9600f36f48ba456233cdfb556edca34af3012c
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-02-27 15:17:08 +0530

    blog file

--- about.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("button").addEventListener("click", signIn);
-
-var regdUsers = JSON.parse(localStorage.getItem("userDatabase"));
-function signIn() {
-  alert("Thank You ");
-}
